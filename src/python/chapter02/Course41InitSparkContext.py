50e0c1620fd4b2944c48260e52a07a4e331adecd
尉勇强2 <hzweiyongqiang@corp.netease.com>
2018-07-16 09:51:37 +0800

    remove python

--- src/python/chapter02/Course41InitSparkContext.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-
-print('初始化SparkContext')
-
-from pyspark import SparkConf,SparkContext
-
-conf = SparkConf().setMaster("local").setAppName("MY App")
-sc = SparkContext(conf = conf)
-print(sc)
