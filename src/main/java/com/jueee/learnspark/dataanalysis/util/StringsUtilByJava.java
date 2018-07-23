ae4f8fc8aed1e0cb57ee660219b1e6574c970f87
尉勇强2 <hzweiyongqiang@corp.netease.com>
2018-07-23 20:22:08 +0800

    Spark 独立应用

---
+++ src/main/java/com/jueee/learnspark/dataanalysis/util/StringsUtilByJava.java
@@ -0,0 +1,15 @@
+package com.jueee.learnspark.dataanalysis.util;
+
+public class StringsUtilByJava {
+    public static String getMethodName(){
+        return new Exception().getStackTrace()[1].getMethodName();
+    }
+
+    public static void printFinish(){
+        System.out.println("[finish]" + new Exception().getStackTrace()[1].getMethodName());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMethodName());
+    }
+}
