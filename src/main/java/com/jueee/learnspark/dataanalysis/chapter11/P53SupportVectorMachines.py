f690e60fedb11096f521bbb14ddbafa441f92fb2
hzweiyongqiang <jue-427299>
2018-12-12 19:59:49 +0800

    支持向量机

--- src/main/java/com/jueee/learnspark/dataanalysis/chapter11/P53SupportVectorMachines.py
+++ src/main/java/com/jueee/learnspark/dataanalysis/chapter11/P53SupportVectorMachines.py
@@ -1,5 +1,5 @@
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+from pyspark.mllib.classification import SVMWithSGD
 from pyspark.mllib.linalg import Vectors
 from pyspark import SparkContext
 
@@ -9,7 +9,7 @@ denseVec2 = LabeledPoint(0.0, Vectors.dense([2.0, 0.0, 1.0]))
 vectors = [denseVec1, denseVec2]
 dataset = sc.parallelize(vectors)
 print(dataset)
-model = LogisticRegressionWithLBFGS.train(dataset, iterations=200, intercept=True)
+model = SVMWithSGD.train(dataset, iterations=200, intercept=True)
 print("weights: %s, intercept: %s" % (model.weights, model.intercept))
 
 # weights: [-5.591575185933013,2.8941330995473336,0.0], intercept: -6.895635245995358
